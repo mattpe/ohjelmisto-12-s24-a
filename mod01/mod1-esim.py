f1fdafe107a89121449bfe58d8ca939b85a5ab4c
mattpe <mattpe@metropolia.fi>
2024-08-20 11:36:42 +0300

    pari esimerkki lisätty

--- mod01/mod1-esim.py
+++ mod01/mod1-esim.py
@@ -1,1 +1,24 @@
-print("Moro");
+"""
+monen
+rivin
+kommmentti on monen rivin merkkijono
+"""
+
+# Tulostetaan juttuja
+# toinen kommenttirivi
+print("""
+Moro
+Matti
+""")
+# \ on escape-merkki
+print("Moro\t Muut")
+# print("Moro Matti")
+
+# Syötteen lukeminen käyttäjältä
+command = "Tee jotain"
+print(command)
+
+command = input("Anna komento: ")
+
+print("Rivinvaihtoa painettu, komento: " + command)
+
