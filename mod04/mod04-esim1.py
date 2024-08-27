74a641295fcd727175cfcbedfaa46c01402f82c5
mattpe <mattpe@metropolia.fi>
2024-08-27 11:26:37 +0300

    Add while loop examples

--- mod04/mod04-esim1.py
+++ mod04/mod04-esim1.py
@@ -17,13 +17,31 @@ print(f"Hurrattiin {hooray_counter} kertaa.")
 """
 
 # noppapeli
-die1 = die2 = counter = 0
-while die1 != 6 or die2 != 6:
-    die1 = random.randint(1, 6)
-    die2 = random.randint(1, 6)
-    counter += 1
-    #print(f"Noppien silmäluvut: {die1}, {die2}, heittojen lkm: {counter}")
-print(f"Heittojen lkm: {counter}")
+
+rounds = 0
+total_throw_count = 0
+while rounds < 10000:
+    rounds += 1
+    die1 = die2 = counter = 0
+    while die1 != 6 or die2 != 6:
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        counter += 1
+        #print(f"Noppien silmäluvut: {die1}, {die2}, heittojen lkm: {counter}")
+    total_throw_count = total_throw_count + counter
+    #print(f"Heittojen lkm tällä kierroksella: {counter}, yhteensä: {total_throw_count}")
+print(f"Heittojen lkm keskiarvo: {total_throw_count/rounds}")
+
+# sisäkkäiset while-silmukat
+eka = 1
+while eka <= 5:
+    print("Ulompi silmukka alkaa")
+    toka = 1
+    while toka <= 5:
+        print("Sisempi silmukka alkaa")
+        print(f"{eka} kertaa {toka} on {eka*toka:d}")
+        toka = toka + 1
+    eka = eka + 1
 
 # "komentorivikäyttöliittymä"
 command = ""
@@ -38,6 +56,14 @@ while command != "lopeta":
             hooray_counter = hooray_counter + 1  # hooray_counter += 1
             print(f"{hooray_counter}. kerran Hurraa!")
         print(f"Hurrattiin {hooray_counter} kertaa.")
+    elif command == "noppa":
+        die1 = die2 = counter = 0
+        while die1 != 6 or die2 != 6:
+            die1 = random.randint(1, 6)
+            die2 = random.randint(1, 6)
+            counter += 1
+            # print(f"Noppien silmäluvut: {die1}, {die2}, heittojen lkm: {counter}")
+        print(f"Heittojen lkm: {counter}")
     elif command == "lopeta":
         print("Lopetetaan ohjelma")
         #break # toistorakenteen suoritus loppuu heti.
