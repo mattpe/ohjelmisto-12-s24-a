32a085c6cf46cbf221a6d3cc134cebdf61455dba
mattpe <mattpe@metropolia.fi>
2024-09-03 11:30:04 +0300

    add more mod 6 function examples

--- mod06/mod6-esim.py
+++ mod06/mod6-esim.py
@@ -81,3 +81,56 @@ def modify_value():
 modify_value()
 print(main_app_var)
 
+
+# Funktiot ja lista parametrina
+# inventory-muuttujaan tallennetaan VIITTAUS olemassaolevaan listaa
+def print_inventory(inventory):
+    print("Pelaajalla on:")
+    for item in inventory:
+        print(item)
+    inventory.append("uusi juttu lisätty funktion sisällä")
+
+player1_inventory = ["veitsi", "vesipullo"]
+player2_inventory = ["kirves", "eväsleipä"]
+
+print_inventory(player1_inventory)
+print_inventory(player2_inventory)
+# tulostus muuttuu, koska funktio muokkasi samaa listaa
+print_inventory(player1_inventory)
+print_inventory(player2_inventory)
+
+
+# sama stringi-arvolla: alkuperäisten muuttujien arvo ei muutu
+def print_inventory_str(inventory):
+    print("Pelaajalla on: " + inventory)
+    inventory += ", uusi juttu lisätty funktion sisällä"
+    print_inventory_str(player1_inventory)
+
+player1_inventory = "veitsi, vesipullo"
+player2_inventory = "kirves, eväsleipä"
+
+print_inventory_str(player1_inventory)
+print_inventory_str(player2_inventory)
+# tulostus ei muutu
+print_inventory_str(player2_inventory)
+
+# extra: nimetyt parametrit ja oletusarvot
+def calc_sum(num1=0, num2=0, num3=0):
+    print(f"Laskutoimitus: {num1} + {num2} + {num3}")
+    return num1 + num2 + num3
+print(calc_sum(2))
+print(calc_sum(2, num3=5))
+print(calc_sum(1, 4, 6))
+
+# extra: vaihtuvanmittainen argumenttijono
+# *nums -> nums muuttujaan muodostuu lista parametreista
+def calc_sum_adv(*nums):
+    total = 0
+    calculation = "Laskutoimitus:"
+    for num in nums:
+        total += num
+        calculation = calculation + " +" + str(num)
+    print(calculation)
+    return total
+
+print(calc_sum_adv(1, 2, 3, 6))
