9c8d2b6cf9b4486f92bc880341f068acb6c5001a
mattpe <mattpe@metropolia.fi>
2024-09-12 10:10:12 +0300

    add python db examples

---
+++ mod08/mod8teht3-start.py
@@ -0,0 +1,8 @@
+from geopy import distance
+
+# select latitude_deg, longitude_deg from airport where ident='EFVU';
+
+efhk_coords = (60.3172, 24.963301)
+efvu_coords = (68.087196, 27.123899)
+result_distance = distance.distance(efhk_coords, efvu_coords)
+print(result_distance)
