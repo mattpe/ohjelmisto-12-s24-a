8041e408ca4fd01ee6549451e6c2953ccd7b3b8b
jormaraty <jorma.raty@metropolia.fi>
2024-10-21 09:59:33 +0300

    Eka versio

---
+++ mod09/Esim1.py
@@ -0,0 +1,20 @@
+'''
+Määritellään Kissa-luokka.
+Luokalla on kolme ominaisuutta.
+'''
+
+class Kissa:
+    # määritellään luokan alustaja
+    def __init__(self, nimi, age):
+        self.nimi = nimi
+        self.ikä = age
+        # määritellään myös kolams ominaisuus
+        self.omistaja = "Tuntematon"
+
+# pääohjelma
+# luodaan uusi Kissa-tyyppinen olio
+ekaKissa = Kissa("Pörri", 3)
+# tulostetaan kisan arvoja
+print(f"Kissan nimi on {ekaKissa.nimi}")
+
+
